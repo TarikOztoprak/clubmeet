322c1571c40076535cbb7edc0ca2c8b4ed4c8553
Tarik <tarik.oztoprak@hotmail.com>
2021-12-11 14:03:00 +0300

    create club

--- screens/ClubsScreen.js
+++ screens/ClubsScreen.js
@@ -1,29 +1,62 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import Channel from '../components/Channel';
 import BottomBar from '../components/BottomBar'
 import { auth } from '../firebase';
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { doc, getDoc } from "firebase/firestore";
+
+function createCode() {
+  let code = '';
+  for (let i = 0; i < 9; i++) {
+    let x = Math.round(Math.random() * 10)
+    code += x
+  }
+  return code
+}
+const db = getFirestore();
+async function CreateClub(params) {
+  try {
+    const docRef = await addDoc(collection(db, "clubs"), {
+      name: params,
+      code: createCode()
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
 
 export default function ClubsScreen({navigation}) {
+    const [cClub, setcClub] = useState('');
     return (
       <View style={styles.homescreen}>
         <View style={styles.banner}><Text style={styles.txt}>Clubs</Text></View>
-        
+    
         <Text>{auth.currentUser?.email}</Text>
         <Text>{auth.currentUser?.uid}</Text>
-        
-        {/* <TouchableOpacity style={styles.create}><Text style={styles.txt}>+ Create Club</Text></TouchableOpacity> */}
+
+        <View style= {styles.row}>
+          <TextInput style= {styles.input}
+              placeholder="Club Name"
+              onChangeText={text => setcClub(text)}
+          />
+          <TouchableOpacity style={styles.create} onPress={()=> CreateClub(cClub)}>
+            <Text style={styles.txt}>Create</Text>
+          </TouchableOpacity>
+        </View>
         
         <View style= {styles.row}>
           <TextInput style= {styles.input}
               placeholder="Club Code"
           />
-          <TouchableOpacity style={styles.create}><Text style={styles.txt}>Join</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.create} onPress={()=> console.log('gelişcek')}><Text style={styles.txt}>Join</Text></TouchableOpacity>
         </View>
         
         <Channel><Text style={styles.txt}>Kitap Kulubü</Text></Channel>
-        <Channel><Text style={styles.txt}>Satranç Kulubü</Text></Channel>
-        
+
         <BottomBar navigation = {navigation}/>
       </View>
      
@@ -84,7 +117,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     paddingLeft: 10,
     paddingRight: 10
-  }
+  },
 });
 
  
